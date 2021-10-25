95f723f774caf3c252b86ca4fdbaff2ae01ea070
Shubham Thorat <thoratshubham048@gmail.com>
2021-10-25 15:15:48 +0530

    update documentation

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# webscrapper-github-topics
-This is a maven java project. It gathers top Github topics and their top 30 repositories in each topic with the help of Jsoup java library and also creates a data frame with help tablesaw 
+# Scraping the repositories on the Topics on Github:
+TODO : 
+Introduction about Web Scrapping
+Introduction about Github Topics and the problem statement
+Mention the tools (Java, Jsoup, table saw, IntelliJ Idea)
+
+OUTLINE:
+1) We are going to scrape from https://github.com/topics -
+2) We will get lists of topics for each topic we will title, topic page URL, and description
+3)For each repo we will grab repo_name, username, repo URL, stars
+4) for each topic we will create csv file format:
+        Repo Name,Username,Stars,Repo URL
+        three.js,mrdoob,669700,https://github.com/mrdoob/three.js
+        libgdx,libgdx,19100,https://github.com/libgdx/libgdx
+        
+
+## Scrape List of Topics from Github
+With the help of Java, the Jsoup library get requests to download the Github topic page
+Use Jsoup. parse() to parse the downloaded page into HTML
+convert HTML into table saw data frame
+
+## Scrape list of Repositories for each topic
+The data frame that we created above get the (topic URL, topic name) for which you want to extract repos
+again, requests to download page of a particular topic
+ parse the download page
+Extracts lists of data (repo_name, username, repo URL, stars) from the page.
+convert it into tablesaw data frame
+
+## Create CSV file for each Topic
+The data frame that we have created for each topic convert into csv file.
+Use csvWriter for creating csv files from each topic data frame
+
+###### ** In main function take input as folder path for storing the csv Files ** 
--- src/main/java/org/example/Scrape.java
+++ src/main/java/org/example/Scrape.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -15,56 +16,55 @@ import java.util.*;
 
 public class Scrape {
 
-
-    public Document get_html_page(String url) throws Exception{
+    public Document get_html_page(String url) throws Exception {
         Document docs = Jsoup.connect(url).get();
         Document html = Jsoup.parse(docs.html());
         return html;
     }
 
-    public ArrayList<String> get_topic_titles(Document code){
-        Elements topic_title =  code.getElementsByClass("f3 lh-condensed mb-0 mt-1 Link--primary");
+    public ArrayList<String> get_topic_titles(Document code) {
+        Elements topic_title = code.getElementsByClass("f3 lh-condensed mb-0 mt-1 Link--primary");
         ArrayList<String> titles = new ArrayList<>();
 
-        topic_title.forEach(e ->{
+        topic_title.forEach(e -> {
             titles.add(e.text());
         });
 
         return titles;
     }
 
-    public ArrayList<String> get_topic_desc(Document code){
-        Elements desc =  code.getElementsByClass("f5 color-text-secondary mb-0 mt-1");
+    public ArrayList<String> get_topic_desc(Document code) {
+        Elements desc = code.getElementsByClass("f5 color-text-secondary mb-0 mt-1");
         ArrayList<String> topic_desc = new ArrayList<>();
 
-        desc.forEach(e ->{
+        desc.forEach(e -> {
             topic_desc.add(e.text());
         });
 
         return topic_desc;
     }
 
-    public ArrayList<String> get_topic_urls(Document code){
+    public ArrayList<String> get_topic_urls(Document code) {
         Elements urls = code.getElementsByClass("f3 lh-condensed mb-0 mt-1 Link--primary");
         ArrayList<String> topic_urls = new ArrayList<>();
 
         String base_url = "https://github.com";
-        urls.forEach(e ->{
+        urls.forEach(e -> {
             topic_urls.add(base_url + e.parent().parent().select("a").attr("href"));
         });
 
         return topic_urls;
     }
 
-    public int[] get_indexes(int size){
+    public int[] get_indexes(int size) {
         int indexes[] = new int[size];
-        for(int i=0;i<size;i++){
+        for (int i = 0; i < size; i++) {
             indexes[i] = i;
         }
         return indexes;
     }
 
-    public Table scrape_topics() throws Exception{
+    public Table scrape_topics() throws Exception {
         String url = "https://github.com/topics";
         Document html = get_html_page(url);
 
@@ -74,21 +74,21 @@ public class Scrape {
         int indexes[] = get_indexes(titles.size());
 
         Table topic_table = Table.create("Topics");
-        DoubleColumn c1 = DoubleColumn.create("Sl_no",indexes);
-        StringColumn c2 = StringColumn.create("Title",titles);
-        StringColumn c3 = StringColumn.create("Description",desc);
-        StringColumn c4 = StringColumn.create("Topic URL",repo_url);
+        DoubleColumn c1 = DoubleColumn.create("Sl_no", indexes);
+        StringColumn c2 = StringColumn.create("Title", titles);
+        StringColumn c3 = StringColumn.create("Description", desc);
+        StringColumn c4 = StringColumn.create("Topic URL", repo_url);
 
-        topic_table.addColumns(c1,c2,c3,c4);
+        topic_table.addColumns(c1, c2, c3, c4);
 
         return topic_table;
     }
 
-    public ArrayList<String> get_repo_owner_names(Document code){
-        Elements repo_owner_names =  code.getElementsByClass("f3 color-fg-muted text-normal lh-condensed");
+    public ArrayList<String> get_repo_owner_names(Document code) {
+        Elements repo_owner_names = code.getElementsByClass("f3 color-fg-muted text-normal lh-condensed");
 
         ArrayList<String> owners = new ArrayList<>();
-        repo_owner_names.forEach(e->{
+        repo_owner_names.forEach(e -> {
             String str[] = e.text().split("/");
             owners.add(str[0].trim());
         });
@@ -96,108 +96,108 @@ public class Scrape {
         return owners;
     }
 
-    public ArrayList<String> get_repo_names(Document code){
-        Elements repo_names =  code.getElementsByClass("f3 color-fg-muted text-normal lh-condensed");
+    public ArrayList<String> get_repo_names(Document code) {
+        Elements repo_names = code.getElementsByClass("f3 color-fg-muted text-normal lh-condensed");
 
         ArrayList<String> names = new ArrayList<>();
-        repo_names.forEach(e->{
+        repo_names.forEach(e -> {
             String str[] = e.text().split("/");
             names.add(str[1].trim());
         });
 
         return names;
     }
-    public ArrayList<String> get_repo_urls(Document code){
-        Elements repo_urls =  code.getElementsByClass("f3 color-fg-muted text-normal lh-condensed");
+
+    public ArrayList<String> get_repo_urls(Document code) {
+        Elements repo_urls = code.getElementsByClass("f3 color-fg-muted text-normal lh-condensed");
 
         ArrayList<String> urls = new ArrayList<>();
         String base_url = "https://github.com/";
-        repo_urls.forEach(e->{
-            String repo_url = base_url +  e.select("a:nth-child(2)").attr("href");
+        repo_urls.forEach(e -> {
+            String repo_url = base_url + e.select("a:nth-child(2)").attr("href");
             urls.add(repo_url);
         });
 
         return urls;
     }
-    public ArrayList<String> get_repo_stars(Document code){
+
+    public ArrayList<String> get_repo_stars(Document code) {
         Elements repo_stars = code.getElementsByClass("social-count float-none");
 
         ArrayList<String> stars = new ArrayList<>();
-        repo_stars.forEach(e->{
+        repo_stars.forEach(e -> {
             stars.add(e.text());
         });
 
         return stars;
     }
 
-    //tasks get repo page - convert into tablesaw return tables and create csv files
-    public Table scrape_topics_repo(String topic_url,String topic_name) throws Exception{
-        Document html = get_html_page(topic_url); //topic repos html
+    // tasks get repo page - convert into tablesaw return tables and create csv
+    // files
+    public Table scrape_topics_repo(String topic_url, String topic_name) throws Exception {
+        Document html = get_html_page(topic_url); // topic repos html
 
         ArrayList<String> repo_owners = get_repo_owner_names(html);
-        ArrayList<String> repo_names= get_repo_names(html);
-        ArrayList<String> repo_urls= get_repo_urls(html);
-        ArrayList<String> repo_stars= get_repo_stars(html);
+        ArrayList<String> repo_names = get_repo_names(html);
+        ArrayList<String> repo_urls = get_repo_urls(html);
+        ArrayList<String> repo_stars = get_repo_stars(html);
         int indexes[] = get_indexes(repo_owners.size());
 
         Table repos = Table.create(topic_name);
-        IntColumn c1 = IntColumn.create("Sl_no",indexes);
-        StringColumn c2 = StringColumn.create("Owners",repo_owners);
-        StringColumn c3 = StringColumn.create("Stars",repo_stars);
-        StringColumn c4 = StringColumn.create("Repo URL",repo_urls);
+        IntColumn c1 = IntColumn.create("Sl_no", indexes);
+        StringColumn c2 = StringColumn.create("Owners", repo_owners);
+        StringColumn c3 = StringColumn.create("Repo Names", repo_names);
+        StringColumn c4 = StringColumn.create("Stars", repo_stars);
+        StringColumn c5 = StringColumn.create("Repo URL", repo_urls);
 
-        repos.addColumns(c1,c2,c3,c4);
+        repos.addColumns(c1, c2, c3, c4, c5);
         return repos;
     }
 
-    public void generateCSV(Table table,String path,String filename) throws IOException{
+    public void generateCSV(Table table, String path, String filename) throws IOException {
         String path_name = path + filename + ".csv";
 
         File file = new File(path_name);
-        if(file.exists() == true){
-            System.out.println("File name exists skipping creation of file..." + filename );
+        if (file.exists() == true) {
+            System.out.println("File name exists skipping creation of file..." + filename);
             return;
         }
 
         CsvWriter writer = new CsvWriter();
-        writer.write(table,new Destination(file));
+        writer.write(table, new Destination(file));
     }
 
-    public void Scrape_Topics_Create_CSV(String pathname) throws Exception{
+    public void Scrape_Topics_Create_CSV(String pathname) throws Exception {
         ScrapeData sc = new ScrapeData();
-        Table topics =  sc.scrape_topics();
+        Table topics = sc.scrape_topics();
 
         File folder = new File(pathname);
 
-        try{
-            if(!folder.mkdirs()){
+        try {
+            if (!folder.mkdirs()) {
                 System.out.println("Folder already exists");
-            }
-            else {
+            } else {
                 System.out.println("Folder created");
             }
-        }
-        catch (Exception e){
-            throw  new Exception(e.getMessage());
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
         }
 
-        try{
-            for(int i=0;i<topics.rowCount();i++){
-                String url = topics.getString(i,"Topic URL");
-                String name = topics.getString(i,"Title");
+        try {
+            for (int i = 0; i < topics.rowCount(); i++) {
+                String url = topics.getString(i, "Topic URL");
+                String name = topics.getString(i, "Title");
                 File file = new File(pathname + name + ".csv");
 
-                if(file.exists() == false){
-                    Table repos = sc.scrape_topics_repo(url,name);
-                    System.out.println("Scraping topic "+ name + " : " + url);
-                    sc.generateCSV(repos,pathname,name);
-                }
-                else{
+                if (file.exists() == false) {
+                    Table repos = sc.scrape_topics_repo(url, name);
+                    System.out.println("Scraping topic " + name + " : " + url);
+                    sc.generateCSV(repos, pathname, name);
+                } else {
                     System.out.println("File already exists skipping..." + name);
                 }
             }
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             System.out.println("Try once again to download remaining files...");
         }
@@ -206,10 +206,7 @@ public class Scrape {
 
 }
 
-
 /*
-        Single function
-        i) get a list of topics from topics page
-        ii) get list top repos for topics
-        ii) create csv for each topic
+ * Single function i) get a list of topics from topics page ii) get list top
+ * repos for topics ii) create csv for each topic
  */
